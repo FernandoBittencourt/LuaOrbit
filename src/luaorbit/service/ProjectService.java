62802e51ae564b5c73c19d3ceb97a2f2c3ffd815
Fernando-A-L-Alves <Fernando-A-L-Alves@users.noreply.github.com>
2017-06-18 17:55:57 -0300

    Increment to ProjectService.java

--- src/luaorbit/service/ProjectService.java
+++ src/luaorbit/service/ProjectService.java
@@ -1,24 +1,38 @@
 package luaorbit.service;
 
+import luaorbit.model;
+
+static Workspace workspace = null;
 
 public class ProjectService {
-	public void createProject(){
-		throw new RuntimeException();
+	public Project createProject(String projectName){
+		return new Project(projectName);
 	}
 	
-	public void deleteProject(){
-		throw new RuntimeException();
+	public void deleteProject(String projectName){
+		List<Project> projects = GetWorkspace().getProjects();
+		projects.removeIf(s -> s.getName().contains(projectName));
 	}
 	
 	public void updateProject(){
 		throw new RuntimeException();
 	}
 	
-	public void importProject(){
+	public void importProject(String sourcePath){
+		//TODO: Convert Files into Project
 		throw new RuntimeException();
 	}
 	
-	public void exportProject(){
+	public void exportProject(String projectName, String destinationPath){
+		List<Project> projects = GetWorkspace().getProjects();
+		Project project = projects.filter(x -> x.getName() == projectName).findFirst();
+		//TODO: Write folders and files
 		throw new RuntimeException();
+	}
+	
+	public Workspace GetWorkspace(){
+		if(workspace == null)
+			workspace = new Workspace();
+		return workspace;
 	}
 }
